9751b02458851f413f80608ed6eb5889c1d689a0
Eugene Yurkevich <eugeney@playtika.com>
2017-05-11 11:40:59 +0300

    fixed Http phpunit tests

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -19,7 +19,7 @@ class HttpTest extends \PHPUnit\Framework\TestCase
 		$this->assertNotEmpty($rules);
 		$this->assertArrayHasKey('disallow', $rules);
 		$this->assertGreaterThan(100, count($rules['disallow']), 'expected more than 100 disallow rules');
-		$this->assertGreaterThan(3, count($rules['sitemap']), 'expected more than 3 sitemaps');
+		$this->assertGreaterThan(2, count($rules['sitemap']), 'expected more than 2 sitemaps');
 	}
 
 	public function testRozetkaComUa()
@@ -34,7 +34,7 @@ class HttpTest extends \PHPUnit\Framework\TestCase
 		$this->assertArrayHasKey('host', $rules);
 		$this->assertGreaterThan(3, count($rules['disallow']), 'expected more than 3 disallow rules');
 		$this->assertNotEmpty($parser->getRules('mediapartners-google'), 'expected Mediapartners-Google rules');
-		$this->assertEquals('rozetka.com.ua', $rules['host']);
+		$this->assertRegexp('!^(https?://)rozetka\.com\.ua$!', $rules['host'], 'strange Host');
 	}
 
 	/**
